74f194095f68a415962268901b0dc39933547958
Ahmed Rady <rady20991@gmail.com>
2024-05-21 00:56:59 +0300

    tryout new cmd

--- console.py
+++ console.py
@@ -42,11 +42,7 @@ class HBNBCommand(cmd.Cmd):
             print(new_instance.id)
         else:
             print("** class doesn't exist **")
-# show: Prints the string representation of an instance based on the class name and id. Ex: $ show BaseModel 1234-1234-1234.
-# If the class name is missing, print ** class name missing ** (ex: $ show)
-# If the class name doesn’t exist, print ** class doesn't exist ** (ex: $ show MyModel)
-# If the id is missing, print ** instance id missing ** (ex: $ show BaseModel)
-# If the instance of the class name doesn’t exist for the id, print ** no instance found ** (ex: $ show BaseModel 121212)
+
     def do_show(self, arg):
         """Prints the string representation of an instance based on the class name and id."""
         arg = arg.split()
@@ -64,25 +60,24 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** no instance found **")
         return
-    
-    # # def do_show(self, arg):
-    # """Prints the string representation of an instance based on the class name and id."""
-    # args = arg.split()
-    # if len(args) == 0:
-    #     print("** class name missing **")
-    # elif len(args) == 1:
-    #     print("** instance id missing **")
-    # elif args[0] not in self.__classes:
-    #     print("** class doesn't exist **")
-    # else:
-    #     dic = models.storage.all()
-    #     key = args[0] + '.' + str(args[1])
-    #     if key in dic:
-    #         print(dic[key])
-    #     else:
-    #         print("** no instance found **")
-    # return
 
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        arg = arg.split()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        else:
+            dic = models.storage.all()
+            key = arg[0] + '.' + str(arg[1])
+            if key in dic:
+                del(dic[key])
+            else:
+                print("** no instance found **")
+        return
 
 
     
