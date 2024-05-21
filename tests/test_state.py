0e19033d1a1fe469145790b5b3aee6d9c9baaccf
Ahmed Rady <rady20991@gmail.com>
2024-05-21 03:31:43 +0300

    state test

--- tests/test_state.py
+++ tests/test_state.py
@@ -21,12 +21,24 @@ class TestState(unittest.TestCase):
         self.assertTrue(issubclass(State, BaseModel))
 
     def test_isinstance(self):
-        """Test if state is instance from State"""
+        """Test if state is an instance of State."""
         self.assertIsInstance(self.state, State)
 
     def test_attribute_types(self):
-        """Test attribute types of the State class"""
+        """Test attribute types of the State class."""
         self.assertIsInstance(self.state.name, str)
+
+    def test_name_attribute(self):
+        """Test the name attribute of State."""
+        self.assertTrue(hasattr(self.state, 'name'))
+        self.assertEqual(self.state.name, "Alexandria")
+
+    def test_missing_attribute(self):
+        """Test that attributes are set correctly even if missing."""
+        del self.state.name
+        self.assertTrue(hasattr(self.state, 'name'))
+        self.state.name = "Cairo"
+        self.assertEqual(self.state.name, "Cairo")
 
 
 if __name__ == '__main__':
